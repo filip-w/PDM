171782e2968044251daac02eb47659c74e67a13c
Wieslander Filip <filip.wieslander@scania.com>
2023-11-14 20:07:33 +0100

    adding int test code

--- PDM.ino
+++ PDM.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
-#include "Adafruit_MAX31855.h"
 #include <mcp2515.h>
 #include <FIR.h>
+#include <ACS712.h>
 
 //General setup
 #define BaseCanID 0x500
@@ -9,18 +9,22 @@
 #define serialDebug false
 #define defaultOutputState HIGH
 
-struct thermocoupleChannel{
-   int channelNumber;
-   Adafruit_MAX31855 thermocoupleFrontend;
-   FIR<double, 10> fir_avg;
+struct OutputChannel{
+   int SwitchOutputChannel;
+   float Ratedcurrent;
+   float timeToBlow;
+   ACS712  ACS;
 };
 
 struct can_frame canMsg1;
 enum CAN_SPEED BaudRate;
-// For a moving average we use all ones as coefficients.
-double coef_avg[10] = {1., 1., 1., 1., 1., 1., 1., 1., 1., 1.};
-byte Lowpass;
 
+volatile bool interrupt = false;
+struct can_frame frame;
+
+void irqHandler() {
+    interrupt = true;
+}
 
 //Digital Pins
 //Digital outputs
@@ -36,9 +40,28 @@ byte Lowpass;
 #define BT_STATE       9
 #define CAN_Interrupt  2 
 
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin5 = A4;  // Battery reference
+const float barRestistorDiv = 1/(1+3.9);
+const int analogInPin6 = A5;  // Analog input 1 (Lower pot on simulator)
+const int analogInPin7 = A6;  // Analog input 2 (high pot on simulator)
+const int analogInPin8 = A7;  // Analog input 3 (NC on simulator)
+
+int sensorValue = 0;  // value read from the pot
+int outputValue = 0;  // value output to the PWM (analog out)
+
 MCP2515 mcp2515(CAN_ChipSelect);
 
 void setup() {
+  OutputChannel CHList[4] = {
+  {3, 5, 1, (A0, 5.0, 1023, 100)},
+  {6, 5, 1, (A1, 5.0, 1023, 100)}, 
+  {5, 5, 1, (A2, 5.0, 1023, 100)}, 
+  {4, 5, 1, (A3, 5.0, 1023, 100)},
+  };
+
+  pinMode(DI1, INPUT_PULLUP);
+  pinMode(DI2, INPUT_PULLUP);
 
   Serial.begin(9600);
 
@@ -73,19 +96,61 @@ void setup() {
   mcp2515.setNormalMode();
   Serial.println("DONE.");
 
-  //Setup digital output pins
-  pinMode(IN_CHN1, OUTPUT);
-  pinMode(IN_CHN2, OUTPUT);
-  pinMode(IN_CHN3, OUTPUT);
-  pinMode(IN_CHN4, OUTPUT);
-  digitalWrite(IN_CHN1, defaultOutputState);
-  digitalWrite(IN_CHN2, defaultOutputState);
-  digitalWrite(IN_CHN3, defaultOutputState);
-  digitalWrite(IN_CHN4, defaultOutputState);
+  for (int i = 0; i <= 4; i++) {
+    pinMode(CHList[i].SwitchOutputChannel, OUTPUT);   //Setup digital output pins
+    digitalWrite(CHList[i].SwitchOutputChannel, defaultOutputState);  // Set Default state
+    //CHList[i].ACS.autoMidPoint(); // Calibrate current sensors
+  } 
+
+  pinMode(CAN_Interrupt, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(CAN_Interrupt), irqHandler, FALLING);
 }
 
 void loop() {
-  mcp2515.sendMessage(&canMsg1);
+  // int mA = ACS.mA_DC();
+  // read the analog in value:
+  // sensorValue = analogRead(analogInPin5);
+  // map it to the range of the analog out:
+  // outputValue = map(sensorValue, 0, 1023, 0, 245); //Scaling for Battery reference
+  // change the analog out value:
+  int reading1 = digitalRead(DI1);
+  int reading2 = digitalRead(DI2);
+  //int reading3 = digitalRead(CAN_Interrupt);
+
+  // print the results to the Serial Monitor:
+  Serial.print("Dig1 = ");
+  Serial.print(reading1);
+  Serial.print(" Dig2 = ");
+  Serial.print(reading2);
+  Serial.print(" Current = ");
+  //Serial.print(mA);
+  Serial.print("\t Battery reference = ");
+  Serial.println(outputValue);
+
+
+  canMsg1.data[0] = reading1;
+
+
+  //mcp2515.sendMessage(&canMsg1);
   //mcp2515.sendMessage(&canMsg2);
   delay(float(1)/UpdateRate*1000); //Crude wait, should use a dynamic time offset depending.
+
+  if (interrupt) {
+    Serial.println("Rx msg!");
+        interrupt = false;
+
+        uint8_t irq = mcp2515.getInterrupts();
+
+        if (irq & MCP2515::CANINTF_RX0IF) {
+            if (mcp2515.readMessage(MCP2515::RXB0, &frame) == MCP2515::ERROR_OK) {
+                // frame contains received from RXB0 message
+            }
+        }
+
+        if (irq & MCP2515::CANINTF_RX1IF) {
+            if (mcp2515.readMessage(MCP2515::RXB1, &frame) == MCP2515::ERROR_OK) {
+                // frame contains received from RXB1 message
+            }
+        }
+    }
 }
